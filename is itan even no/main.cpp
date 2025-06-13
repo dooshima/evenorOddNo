2416d95cb0469e2ac15fd8d09a9feac1e758b4f7
Anger Dooshima Lois <danielladoosh@gmail.com>
2025-06-13 01:09:52 +0100

    Update main.cpp

--- is itan even no/main.cpp
+++ is itan even no/main.cpp
@@ -3,20 +3,26 @@ using namespace std;
 
 int main() {
 
-	int anyNumber, oddOrEvenNo;
+	int anyNumber;
 
+	//long hand codes
 	cout << "Enter any number of your choice : ";
 	cin >> anyNumber;
 
-	
+  	
 	if (anyNumber % 2 == 0) {
-		cout << "Its an even no";
+		cout << "Its an even no"<< std::endl;
 
 	}
 	else {
-		cout << "Its an odd no";
+		cout << "Its an odd no"<< std::endl;
 
 	}
+
+	//short hand
+	cout << "Enter any number of your choice : ";
+	cin >> anyNumber;
+	cout << (anyNumber & 1 ? "Odd" : "Even") << endl;
 
 
 	return 0;
